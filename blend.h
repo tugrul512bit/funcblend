6226cf650966f6c06b1e18b348fed3509f18beb1
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2023-03-05 19:39:58 +0300

    Update blend.h

--- blend.h
+++ blend.h
@@ -11,13 +11,22 @@ double blend1(Func1 f1, Func2 f2,double blendPoint, double x)
     return result;
 }
 
-// blending takes narrower or wider distance to reach f1 or f2
+// function-blending takes narrower or wider distance to reach f1 or f2
 template<typename Func1, typename Func2>
 double blend2(Func1 f1, Func2 f2,double blendPoint, double transitionSmoothness, double x)
 {
     double mult1 =0.5 + 0.5 * std::tanh((blendPoint - x)/transitionSmoothness);
     double mult2 =1.0-mult1;
     double result = f1(x) * mult1 + f2(x) * mult2;
+    return result;
+}
+
+// blending is applied directly on return values of functions
+double blend3(double f1, double f2,double blendPoint, double transitionSmoothness, double x)
+{
+    double mult1 =0.5 + 0.5 * std::tanh((blendPoint - x)/transitionSmoothness);
+    double mult2 =1.0-mult1;
+    double result = f1 * mult1 + f2 * mult2;
     return result;
 }
 
@@ -95,6 +104,17 @@ int main()
                         [](double input){ return input * 2.0;},
                         5.0,
                         0.05,
-                        x) << std::endl;                            
+                        x) << std::endl;            
+                        
+    std::cout<<"---------------------------------"<<std::endl;                        
+    
+    
+    for(double x = 0;x<10; x+=0.1)
+        std::cout << x<<":::" << blend3(
+                        x,
+                        x*2.0,
+                        5.0,
+                        0.05,
+                        x) << std::endl;                             
  return 0;   
 }
