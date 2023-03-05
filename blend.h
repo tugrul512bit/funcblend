fec4a658db7352d85bdda03f555947dd60bc7725
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2023-03-05 19:29:50 +0300

    Update blend.h

--- blend.h
+++ blend.h
@@ -11,6 +11,16 @@ double blend1(Func1 f1, Func2 f2,double blendPoint, double x)
     return result;
 }
 
+
+template<typename Func1, typename Func2>
+double blend2(Func1 f1, Func2 f2,double blendPoint, double transitionSmoothness, double x)
+{
+    double mult1 =0.5 + 0.5 * std::tanh((blendPoint - x)/transitionSmoothness);
+    double mult2 =1.0-mult1;
+    double result = f1(x) * mult1 + f2(x) * mult2;
+    return result;
+}
+
 int main()
 {
     for(double x = 0;x<10; x+=0.1)
@@ -28,5 +38,40 @@ int main()
                         [](double input){ return input * 2.0;},
                         5.0,
                         x) << std::endl;    
+                        
+                        
+    std::cout<<"---------------------------------"<<std::endl;                        
+    
+    
+    for(double x = 0;x<10; x+=0.1)
+        std::cout << x<<":::" << blend2(
+                        [](double input){ return input ; },
+                        [](double input){ return input * 2.0;},
+                        5.0,
+                        2.0,
+                        x) << std::endl;       
+                        
+    std::cout<<"---------------------------------"<<std::endl;                        
+    
+    
+    for(double x = 0;x<10; x+=0.1)
+        std::cout << x<<":::" << blend2(
+                        [](double input){ return input ; },
+                        [](double input){ return input * 2.0;},
+                        5.0,
+                        4.0,
+                        x) << std::endl;        
+                        
+                        
+    std::cout<<"---------------------------------"<<std::endl;                        
+    
+    
+    for(double x = 0;x<10; x+=0.1)
+        std::cout << x<<":::" << blend2(
+                        [](double input){ return input ; },
+                        [](double input){ return input * 2.0;},
+                        5.0,
+                        0.2,
+                        x) << std::endl;                            
  return 0;   
 }
